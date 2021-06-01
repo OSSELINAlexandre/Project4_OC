6885143b43c133a50978d249bb9b3306a8505586
Alexandre Osselin <alexandre.osselin@gmail.com>
2021-06-01 10:24:10 +0200

    Fixing the UNIT test by changing the methods in FareCalculatorService

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -16,11 +16,23 @@ public class FareCalculatorService {
 	// will count 1 hour.
 	// In the same vein, if you arrived at 9h00 and get out at 9h59, the system will
 	// count 0 hour.
-	int inHour = ticket.getInTime().getHours();
-	int outHour = ticket.getOutTime().getHours();
 
-	// TODO: Some tests are failing here. Need to check if this logic is correct
-	int duration = outHour - inHour;
+	// SOLUTION : Instead of taking the HOUR, we take the time (see JavaDoc on Date)
+	// We substract the OutTime by the InTime, and we transform that number to a
+	// unit in hour as a Double.
+	// For Instance, if you came in at 9h30 am and get out at 10h00 am in the same
+	// day,
+	// the code will count the duration as 0.5, which makes it easier to count the
+	// fare
+	// of the ticket.
+	long inHour = ticket.getInTime().getTime();
+	long outHour = ticket.getOutTime().getTime();
+
+	// Because the getTime() method transform the time in MilliSeconds, we have,
+	// with the division, to
+	// transform the unit in a Hour unit. (There is 1000 ms in a second, 60 seconds
+	// in a minute, 60 minutes in an hour).
+	double duration = (outHour - inHour) / (1000.0 * 60.0 * 60.0);
 
 	switch (ticket.getParkingSpot().getParkingType()) {
 	case CAR: {
