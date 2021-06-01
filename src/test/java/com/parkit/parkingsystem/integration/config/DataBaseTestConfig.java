8628d6f360ec721ac9f717338e555054a6445877
Alexandre Osselin <alexandre.osselin@gmail.com>
2021-06-01 10:31:33 +0200

    Fixing second bug, adding a Time Zone adaptator to the URL of the DB

--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
@@ -10,12 +10,18 @@ public class DataBaseTestConfig extends DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseTestConfig");
 
-// The first issue here come from the adress and the password used.
+// A - The first issue here come from the adress and the password used.
 //In my system, the password is different, therefore I need to change it to Sookie
+
+// B - Issue with the time zone value " Paris, Madrid is unrecognized or represents more than one time zone.
+// You must configure either the server or JDBC driver
+// Based on Oracle Doc solution, I added the following ?serverTimezone=UTC at
+// the end of the URL
+
     public Connection getConnection() throws ClassNotFoundException, SQLException {
 	logger.info("Create DB connection");
 	Class.forName("com.mysql.cj.jdbc.Driver");
-	return DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "Sookie");
+	return DriverManager.getConnection("jdbc:mysql://localhost:3306/test?serverTimezone=UTC", "root", "Sookie");
     }
 
     public void closeConnection(Connection con) {
